506d1e2a863558371127bab283c55c8985f80912
Vilson Vieira <vilson@void.cc>
2014-03-31 21:50:58 -0300

    basic waveform interaction: play specific offsets while dragging or touching the canvas

--- js/lamina.js
+++ js/lamina.js
@@ -2,23 +2,6 @@
 (function() {
   var Sample, audioContext, c, canvas, demoSample, hammer, onFrame;
 
-  window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
-
-  canvas = document.getElementById("c");
-
-  canvas.width = 500;
-
-  canvas.height = 500;
-
-  c = canvas.getContext("2d");
-
-  hammer = Hammer(window.document.body).on("tap", function(event) {
-    console.log("tap");
-    demoSample.stop();
-  });
-
-  audioContext = new AudioContext();
-
   Sample = (function() {
     function Sample(url, onReady) {
       var data, source;
@@ -54,7 +37,6 @@
       this.source = audioContext.createBufferSource();
       this.source.buffer = this.data;
       this.source.connect(audioContext.destination);
-      console.log(duration);
       return this.source.start(time, offset, duration);
     };
 
@@ -65,30 +47,61 @@
       return this.source.stop(time);
     };
 
-    Sample.prototype.drawOn = function(canvas) {
-      var amp, data, height, i, step, width, _i;
+    Sample.prototype.drawOn = function(canvas, head) {
+      var amp, c, data, height, i, step, width, _i;
       width = canvas.width;
       height = canvas.height;
       data = this.data.getChannelData(0);
       step = Math.ceil(data.length / width);
-      amp = 200;
+      amp = height;
       c = canvas.getContext("2d");
+      c.clearRect(0, 0, width, height);
       c.beginPath();
       c.moveTo(0, height / 2 + data[0]);
       for (i = _i = 0; 0 <= width ? _i <= width : _i >= width; i = 0 <= width ? ++_i : --_i) {
         c.lineTo(i, height / 2 + data[step * i] * amp);
       }
       c.closePath();
-      return c.stroke();
+      c.strokeStyle = "green";
+      c.stroke();
+      if (head) {
+        c.beginPath();
+        c.moveTo(head, 0);
+        c.lineTo(head, height);
+        c.closePath();
+        c.strokeStyle = "blue";
+        return c.stroke();
+      }
     };
 
     return Sample;
 
   })();
 
+  window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+
+  canvas = document.getElementById("c");
+
+  canvas.width = window.innerWidth;
+
+  canvas.height = window.innerHeight;
+
+  c = canvas.getContext("2d");
+
+  audioContext = new AudioContext();
+
+  hammer = Hammer(canvas).on("drag touch", function(event) {
+    var offsetSeconds, step, x;
+    step = Math.ceil(demoSample.data.length / canvas.width);
+    x = event.gesture.center.clientX;
+    offsetSeconds = x * step / audioContext.sampleRate;
+    demoSample.stop();
+    demoSample.play(0, offsetSeconds);
+    demoSample.drawOn(canvas, x);
+  });
+
   demoSample = new Sample("samples/demo.wav", function() {
     this.drawOn(canvas);
-    this.play(0, 3);
   });
 
   onFrame = function(timestamp) {
--- src/lamina.coffee
+++ src/lamina.coffee
@@ -1,21 +1,4 @@
-window.requestAnimationFrame =
-  window.requestAnimationFrame or
-  window.mozRequestAnimationFrame or
-  window.webkitRequestAnimationFrame or
-  window.msRequestAnimationFrame
-
-canvas = document.getElementById "c"
-canvas.width = 500
-canvas.height = 500
-c = canvas.getContext "2d"
-
-hammer = Hammer(window.document.body).on "tap", (event) ->
-  console.log "tap"
-  demoSample.stop()
-  return
-
-audioContext = new AudioContext()
-
+# Abstracting how to load, play and draw sample files
 class Sample
   constructor: (url, onReady) ->
     data = null
@@ -37,31 +20,67 @@ class Sample
     @source = audioContext.createBufferSource()
     @source.buffer = @data
     @source.connect(audioContext.destination)
-    console.log(duration)
     @source.start(time, offset, duration)
 
   stop: (time=0) ->
     @source.stop(time)
             
-  drawOn: (canvas) ->
+  drawOn: (canvas, head) ->
     width = canvas.width
     height = canvas.height
     data = @data.getChannelData(0)
     step = Math.ceil(data.length / width)
-    amp = 200
+    amp = height
     c = canvas.getContext "2d"
+    c.clearRect(0, 0, width, height)
     c.beginPath()
     c.moveTo(0, height/2 + data[0])
     for i in [0..width]
       c.lineTo(i, height/2 + data[step*i] * amp)
     c.closePath()
+    c.strokeStyle = "green"
     c.stroke()
 
-demoSample = new Sample "samples/demo.wav", () ->
-  @drawOn(canvas)
-  @play(0, 3)
+    if head
+      c.beginPath()
+      c.moveTo(head, 0)
+      c.lineTo(head, height)
+      c.closePath()
+      c.strokeStyle = "blue"
+      c.stroke()
+
+# Multi-brower requestAnimationFrame
+window.requestAnimationFrame =
+  window.requestAnimationFrame or
+  window.mozRequestAnimationFrame or
+  window.webkitRequestAnimationFrame or
+  window.msRequestAnimationFrame
+
+# Waveform canvas setup
+canvas = document.getElementById "c"
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+c = canvas.getContext "2d"
+
+# Audio setup
+audioContext = new AudioContext()
+
+# Gesture events
+hammer = Hammer(canvas).on "drag touch", (event) ->
+  step = Math.ceil(demoSample.data.length / canvas.width)
+  x = event.gesture.center.clientX
+  offsetSeconds = x*step / audioContext.sampleRate
+  demoSample.stop()
+  demoSample.play(0, offsetSeconds)
+  demoSample.drawOn(canvas, x)
   return
 
+# Main
+demoSample = new Sample "samples/demo.wav", () ->
+  @drawOn(canvas)
+  return
+
+# Animation loop
 onFrame = (timestamp) ->
   requestAnimationFrame(onFrame)
   return
